357667237f05930b2033394acabee25e4d4cf456
Aleks <ajrudzitis@users.noreply.github.com>
2024-05-04 12:50:30 -0700

    sample menu

--- app/app.go
+++ app/app.go
@@ -8,23 +8,32 @@ import (
 )
 
 func Run(pty *pty.Pty) {
+	// create a new terminfo screen
 	screen, err := tcell.NewTerminfoScreenFromTty(pty)
 	if err != nil {
 		log.Errorf("app: failed to create terminfo screen: %v", err)
 		return
 	}
 
+	// create a new application and set the screen
 	app := tview.NewApplication()
 	app.SetScreen(screen)
-	form := tview.NewForm().
-		AddInputField("Enter Card Number", "", 16, nil, nil).
-		AddButton("Submit", nil).
-		AddButton("Quit", func() {
+
+	menu := tview.NewList().
+		AddItem("List item 1", "Some explanatory text", 'a', nil).
+		AddItem("List item 2", "Some explanatory text", 'b', nil).
+		AddItem("List item 3", "Some explanatory text", 'c', nil).
+		AddItem("List item 4", "Some explanatory text", 'd', nil).
+		AddItem("Quit", "Press to exit", 'q', func() {
 			app.Stop()
 		})
-	form.SetBorder(true).SetTitle("Enter Payment Details").SetTitleAlign(tview.AlignLeft)
-	if err := app.SetRoot(form, true).Run(); err != nil {
-		log.Errorf("failed to run app: %v", err)
+
+	window := tview.NewFrame(menu).
+		SetBorders(2, 2, 2, 2, 4, 4).
+		AddText("Aleks's Resume", true, tview.AlignCenter, tcell.ColorWhite)
+
+	if err := app.SetRoot(window, true).SetFocus(menu).Run(); err != nil {
+		panic(err)
 	}
 
 }
